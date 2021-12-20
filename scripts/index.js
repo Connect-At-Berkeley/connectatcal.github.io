2673ef8b3395a432cca25926711c0a91f612aeeb
Dana Feng <danafeng@berkeley.edu>
2021-12-19 17:40:41 -0800

    finishing touches

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,17 @@ window.smoothScroll = function(target) {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+window.addEventListener("scroll", reveal);
